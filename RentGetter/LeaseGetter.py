06eb939c9d93c5341469fae9d31f2290d26431e8
RickyDing <jiaqiding.ricky@gmail.com>
2019-12-05 17:56:43 +0800

    租房所有信息都爬到了，晚上数据库

--- RentGetter/LeaseGetter.py
+++ RentGetter/LeaseGetter.py
@@ -5,9 +5,8 @@ from bs4 import BeautifulSoup
 import time
 import re
 
-community_key = ["小区名称", "小区房价", "小区房价增长率", "区域商圈", "详细地址", "建筑类型", "物业费用", "产权类别", "容积率", "总户数", "绿化率", "建筑年代",
-                  "停车位", "开发商", "物业公司", "在租房源", "在售房源"]
-house_key = ["标题描述", "房租", "整租合租", "面积", "户型", "朝向", "装修情况", "楼层", "小区名称","帖数","所在地址", "个人/经纪人", "房屋描述"]
+community_key = ["小区名称", "小区均价", "小区房价增长率", "区域商圈", "详细地址", "建筑类型", "物业费用", "产权类别", "容积率", "总户数", "绿化率", "建筑年代", "停车位", "开发商", "物业公司", "在租房源", "在售房源"]
+house_key = ["标题", "房租", "户型", "整租合租", "面积", "朝向", "楼层", "装修", "小区名称", "帖数", "详细地址", "个人/经纪人", "房屋描述"]
 
 
 def get_proxy():
@@ -35,13 +34,17 @@ def request(url):
     while True:
         try:
             r = requests.get(url, timeout=3, headers=headers, proxies=get_proxy())
-            while (r.text.strip() == '') or ('Forbidden'in r.text):
-                print('请求结果不含所需信息')
+            while r.text.strip() == '':
+                print('请求结果为空')
                 time.sleep(0.3)
                 r = requests.get(url, timeout=3, headers=headers, proxies=get_proxy())
-            while '访问过于频繁' in r.text :
+            while 'Forbidden'in r.text:
+                print('403 Forbidden')
+                time.sleep(0.3)
+                r = requests.get(url, timeout=3, headers=headers, proxies=get_proxy())
+            while '访问过于频繁' in r.text:
                 print(url)
-                print('程序暂停，请访问链接进行人机验证！')
+                print('访问过于频繁，请访问链接进行人机验证！')
                 os.system("pause")
                 r = requests.get(url, timeout=3, headers=headers, proxies=get_proxy())
             return r
@@ -56,7 +59,6 @@ def request(url):
 
 
 def get_community(city):
-    # 进入城市首页链接
     index_r = request('http://www.ganji.com/index.htm')
     index_bs = BeautifulSoup(index_r.text, 'lxml')
     city_href = index_bs.find('a', text=city)['href']
@@ -93,25 +95,44 @@ def get_community(city):
                     # 标题及房价
                     house_title = re.sub(r'\s+', ' ', house_bs.select_one('.card-top .card-title').text).strip()
                     house_price = re.sub(r'\s+', ' ', house_bs.select_one('span.price').text)
-                    # 整租 面积 朝向 楼层 装修
+                    # 户型 整租 面积 朝向 楼层 装修
                     house_value_list1 = [re.sub(r'\s+', ' ', x.get_text(strip=True)) for x in house_bs.select('li.item.f-fl .content')]
                     house_value_list = [house_title, house_price] + [house_value_list1[0]] + re.split(r'\s+', house_value_list1[1]) + house_value_list1[2:]
                     # 小区名称 链接
                     community_link_node = house_bs.select_one('.er-item .content a')
                     if community_link_node:
                         community_link = community_link_node['href']
+                        # 进入小区详情页面
+                        community_r = request(community_link)
+                        community_bs = BeautifulSoup(community_r.text, 'lxml')
+                        # 小区名称 小区均价 价格走势
+                        community_title = community_bs.select_one('.card-top .card-title')['title']
+                        community_price = community_bs.select_one('span.price').contents[0]
+                        community_contrast = community_bs.select_one('div .contrast').contents[0]
+                        # 在租 在售信息
+                        community_value_list1 = [re.sub(r'\s+', ' ', attr.get_text(strip=True)) for attr in community_bs.select('li.item.f-fl .content')]
+                        community_rent = re.findall(r'\d+', community_bs.select('.xq-card-resource span a')[0].get_text(strip=True))[0]
+                        community_sale = re.findall(r'\d+', community_bs.select('.xq-card-resource span a')[1].get_text(strip=True))[0]
+                        # 小区属性列表 ["小区名称", "小区均价", "价格走势", "区域商圈", "详细地址", "建筑类型", "物业费用", "产权类别", "容积率", "总户数", "绿化率", "建筑年代", "停车位", "开发商", "物业公司", "在租数", "在售数"]
+                        community_value_list = [community_title, community_price, community_contrast] + community_value_list1 + [community_rent] + [community_sale]
+                        print(community_value_list)
+                        # 小区名称
                         community_name = community_link_node.get_text(strip=True)
-                        # 帖数
+                        # 帖数 详细地址
                         house_value_list2 = house_bs.select('.er-item .content')
                         post_num = re.findall(r'\d+', house_value_list2[0].get_text(strip=True))[0]
                         house_address = re.sub(r'\s+', '', house_value_list2[2].text)
                     else:
                         house_value_list3 = house_bs.select('.er-item .content')
+                        # 帖数 小区名称 详细地址
                         post_num = 'unknown'
                         community_name = house_value_list3[0].get_text(strip=True)
                         house_address = re.sub(r'\s+', '', house_value_list3[2].text)
+                    # 经纪人/个人 房屋描述
                     agent = "company" if house_bs.select_one('.user-info-top .license_box') else "individual"
-                    house_value_list.extend([community_name, post_num, house_address, agent])
+                    house_comment = re.sub(r'\s+', ' ', house_bs.select_one('.describe .item').text)
+                    # 房屋属性列表 ["标题", "房租", "户型", "整租合租", "面积", "朝向","楼层", "装修", "小区名称","帖数","详细地址", "个人/经纪人", "房屋描述"]
+                    house_value_list.extend([community_name, post_num, house_address, agent,house_comment])
                     print(house_value_list)
                 next = post_list_bs.find('a', text='下一页')
                 if next is None:
