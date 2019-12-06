484509076fb47d9e5c9eaa4e92abf1e010c42fde
RickyDing <jiaqiding.ricky@gmail.com>
2019-12-06 20:43:49 +0800

    使用收费代理

---
+++ RentGetter/PaidRequests.py
@@ -0,0 +1,52 @@
+import requests
+import time
+import os
+
+
+def request(url):
+    proxy_host = 'dyn.horocn.com'
+    proxy_port = 50000
+    proxy_username = '7IEY1652172886030494'
+    proxy_pwd = "On5xSrhkNlykfo4G"
+    proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
+        "host": proxy_host,
+        "port": proxy_port,
+        "user": proxy_username,
+        "pass": proxy_pwd,
+    }
+    proxies = {
+        'http': proxyMeta,
+        'https': proxyMeta,
+    }
+    headers = {
+        'user-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36'
+    }
+    url = str(url)
+    if url.startswith('//'):
+        url = url.replace('//', 'http://', 1)
+    while True:
+        try:
+            r = requests.get(url, timeout=3, headers=headers, proxies=proxies)
+            while r.status_code != 200:
+                print('状态码不为200')
+                time.sleep(0.1)
+                r = requests.get(url, timeout=3, headers=headers, proxies=proxies)
+            while r.text.strip() == '':
+                print('请求结果为空')
+                time.sleep(0.1)
+                r = requests.get(url, timeout=3, headers=headers, proxies=proxies)
+            while '访问过于频繁' in r.text:
+                print(url)
+                print('访问过于频繁，请访问链接进行人机验证！')
+                os.system("pause")
+                r = requests.get(url, timeout=3, headers=headers, proxies=proxies)
+            return r
+        except Exception as e:
+            print(url)
+            print(str(e))
+            print('获取页面内容时出错了')
+
+
+if __name__ == "__main__":
+    while True:
+        request("http://www.baidu.com")
